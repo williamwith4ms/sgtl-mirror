533f674647494a90de96a25974b74d73dba31500
williammmm <me@williammmm.dev>
2025-12-09 22:27:54 +0000

    add echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, builder::Str};
+mod methods;
+use methods::*;
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// verbose mode
+    #[arg(short, long)]
+    verbose: bool,
+
+    #[arg(short, long)]
+    decode: bool,
+
+    method: String,
+
+    data: String,
+
 }
+
+fn main() {
+    let args = Args::parse();
+
+    if args.verbose {
+        println!("Method: {}", args.method);
+        println!("Data: {}", args.data);
+        println!("Decode: {}", args.decode);
+    }
+
+    match args.method.as_str() {
+        "echo" => {
+            let result = methods::echo::echo(&args.data);
+            println!("{}", result);
+        },
+        _ => {
+            eprintln!("Unknown method: {}", args.method);
+        }
+    }
+}
+
