25709001ca381defa62d099e6280c5c41576bb92
williammmm <me@williammmm.dev>
2025-12-10 00:49:40 +0000

    add help messages to commands

--- src/main.rs
+++ src/main.rs
@@ -24,33 +24,77 @@ struct Args {
 
 #[derive(Subcommand, Debug, PartialEq)]
 enum Method {
-    #[command(name = "echo", about = "Returns the input string unchanged", long_about = None, after_help = "Example: sgtl echo -d 'Hello, world!'")]
+    #[command(
+        name = "echo", 
+        about = "Returns the input unchanged", 
+        long_about = None, 
+        after_help = "Example: sgtl echo -d 'Hello, world!'")]
     Echo {
         data: Option<String>,
     },
+    #[command(
+        name = "rot26", 
+        about = "Applies ROT26 cipher (no change)", 
+        long_about = "Applies ROT26 cipher (no change), this is actually just an alias for echo.", 
+        after_help = "Example: sgtl rot26 -d 'Hello, world!'")]
     Rot26 {
         data: Option<String>,
     },
+    #[command(
+        name = "base64", 
+        about = "Encodes/Decodes Base64", 
+        long_about = None, 
+        after_help = "Example: sgtl base64 -d 'Hello, world!'")]
     Base64 {
         data: Option<String>,
     },
+    #[command(
+        name = "sha256", 
+        about = "Computes SHA-256 hash", 
+        long_about = None, 
+        after_help = "Example: sgtl sha256 -d 'Hello, world!'")]
     Sha256 {
         data: Option<String>,
     },
+    #[command(
+        name = "sha512", 
+        about = "Computes SHA-512 hash", 
+        long_about = None, 
+        after_help = "Example: sgtl sha512 -d 'Hello, world!'")]
     Sha512 {
         data: Option<String>,
     },
+    #[command(
+        name = "sha384", 
+        about = "Computes SHA-384 hash", 
+        long_about = None, 
+        after_help = "Example: sgtl sha384 -d 'Hello, world!'")]
     Sha384 {
         data: Option<String>,
     },
+    #[command(
+        name = "sha224", 
+        about = "Computes SHA-224 hash", 
+        long_about = None, 
+        after_help = "Example: sgtl sha224 -d 'Hello, world!'")]
     Sha224 {
         data: Option<String>,
     },
+    #[command(
+        name = "sha512_256", 
+        about = "Computes SHA-512/256 hash", 
+        long_about = None, 
+        after_help = "Example: sgtl sha512_256 -d 'Hello, world!'")]
     Sha512_256 {
         data: Option<String>,
     },
+    #[command(
+        name = "caesar", 
+        about = "Applies Caesar cipher with a specified shift", 
+        long_about = None, 
+        after_help = "Example: sgtl caesar 3 'Hello, world!'")]
     Caesar {
-        additional: i8,
+        shift: i8,
         data: Option<String>,
     },
 }
@@ -71,9 +115,11 @@ impl Method {
     }
 }
 
+
 fn get_data(args: &Args) -> String {
     let file = &args.input_file;
     let data = args.method.data();
+
 
     match (file, data) {
         (Some(_), Some(_)) => {
@@ -88,6 +134,7 @@ fn get_data(args: &Args) -> String {
         }
     }
 }
+
 
 fn main() {
     let args: Args = Args::parse();
@@ -105,9 +152,7 @@ fn main() {
     }
 
     let output: String = match &args.method {
-        Method::Echo { data: _ } | Method::Rot26 { data: _ } => {
-            methods::echo::echo(&input_data).to_string()
-        }
+        Method::Echo { data: _ } | Method::Rot26 { data: _ } => methods::echo::echo(&input_data).to_string(),
         Method::Base64 { data: _ } => {
             if args.decode {
                 match methods::base64::base64_decode(&input_data) {
@@ -126,14 +171,11 @@ fn main() {
         Method::Sha384 { data: _ } => methods::sha2::sha384_hash(&input_data),
         Method::Sha224 { data: _ } => methods::sha2::sha224_hash(&input_data),
         Method::Sha512_256 { data: _ } => methods::sha2::sha512_256_hash(&input_data),
-        Method::Caesar {
-            data: _,
-            additional,
-        } => {
+        Method::Caesar { data: _, shift } => {
             if args.decode {
-                methods::caesar::caesar_decipher(&input_data, *additional)
+                methods::caesar::caesar_decipher(&input_data, *shift)
             } else {
-                methods::caesar::caesar_encipher(&input_data, *additional)
+                methods::caesar::caesar_encipher(&input_data, *shift)
             }
         }
     };
@@ -172,7 +214,7 @@ mod tests {
             input_file: None,
             output_file: None,
             method: Method::Caesar {
-                additional: 5,
+                shift: 5,
                 data: Some("Caesar data".to_string()),
             },
         };
@@ -202,9 +244,7 @@ mod tests {
             decode: false,
             input_file: Some("test.txt".to_string()),
             output_file: None,
-            method: Method::Echo {
-                data: Some("Test data".to_string()),
-            },
+            method: Method::Echo { data: Some("Test data".to_string()) },
         };
         let result = std::panic::catch_unwind(|| {
             get_data(&args);
