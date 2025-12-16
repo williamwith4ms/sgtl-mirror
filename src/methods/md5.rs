3ee1839e06af0bf978845852232db300cc9fe6b1
williammmm <me@williammmm.dev>
2025-12-16 10:21:24 +0000

    add md5 tests

--- src/methods/md5.rs
+++ src/methods/md5.rs
@@ -4,3 +4,15 @@ pub fn md5_hash(input: &str) -> String {
     let digest = compute(input);
     format!("{:x}", digest)
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_md5_hash() {
+        let input = "Hello, world!";
+        let expected = "6cd3556deb0da54bca060b4c39479839";
+        assert_eq!(md5_hash(input), expected);
+    }
+}
